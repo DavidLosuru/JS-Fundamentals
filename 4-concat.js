33fe4cb69c23e1d3375c68c11cf7d744669668ad
DavidLosuru <92568294+DavidLosuru@users.noreply.github.com>
2025-09-20 14:34:50 +0300

    Delete 4-concat.js
    did match assignment requirement

--- 4-concat.js
+++
@@ -1,7 +0,0 @@
-// Define a function to print two arguments in the specified format
-function printArguments(arg1, arg2) {
-  console.log(`${arg1} is ${arg2}`);
-}
-
-// Call the function with two example arguments
-printArguments("John", "Developer");
