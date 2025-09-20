c69e398c98a37d347f7278207fbf7082b450abcb
David Losuruq <losurudavid@gmail.com>
2025-09-20 14:27:36 +0300

    4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,7 @@
+// Define a function to print two arguments in the specified format
+function printArguments(arg1, arg2) {
+  console.log(`${arg1} is ${arg2}`);
+}
+
+// Call the function with two example arguments
+printArguments("John", "Developer");
