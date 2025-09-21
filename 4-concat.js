e4d8a3e3c9ecd86b1530d41669c117bbde63f978
David Losuruq <losurudavid@gmail.com>
2025-09-21 12:41:46 +0300

    4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,16 +1,3 @@
-#!/usr/bin/node
-
-function printArgs() {
-  const args = process.argv.slice(2);
-  if (args.length === 2) {
-    console.log(`${args[0]} is ${args[1]}`);
-  } else if (args.length === 1) {
-    console.log(`${args[0]}`);
-  } else {
-    console.log();
-  }
-}
-
 printArgs();
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
