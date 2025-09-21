1108bc078d378246c6ab69ca6a021ac06daea3cd
David Losuruq <losurudavid@gmail.com>
2025-09-21 13:14:25 +0300

    4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,10 @@
+
 #!/usr/bin/node
 
 function printArgs() {
-  if (process.argv.length === 3) {
+  if (process.argv.length === 2) {
+    console.log("undefined is undefined");
+  } else if (process.argv.length === 3) {
     console.log(`${process.argv[2]} is undefined`);
   } else if (process.argv.length > 3) {
     console.log(`${process.argv[2]} is ${process.argv[3]}`);
