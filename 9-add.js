70f9bd9046c707fd6326e2dda4ae9f7f9e0e7021
David Losuruq <losurudavid@gmail.com>
2025-09-21 20:49:37 +0300

    9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,6 +1,19 @@
+#!/usr/bin/node
+
 function add(a, b) {
-  console.log(a + b);
+  if (isNaN(a) || isNaN(b)) {
+    console.log('NaN');
+  } else {
+    console.log(a + b);
+  }
 }
 
-// Example usage:
-add(5, 10);
+const args = process.argv.slice(2).map(Number);
+
+if (args.length === 0) {
+  console.log('NaN');
+} else if (args.length === 1) {
+  add(args[0], 0);
+} else {
+  add(args[0], args[1]);
+}
